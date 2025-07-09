a02a355ffc8c98a1d022bf075939b7d43dd5dc0a
Ranjith Kumar M <rkranjan1918@gmail.com>
2025-07-09 17:27:28 +0530

    V1 commit

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  
+isNavbarCollapsed = true; // Controls the 'show' class
 
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  collapseNavbar() {
+    this.isNavbarCollapsed = true;
+  }
 }
